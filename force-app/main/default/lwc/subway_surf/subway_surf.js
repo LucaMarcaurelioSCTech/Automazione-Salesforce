5b17e739c16216d3c2f5b15d41114c24b5ed8726
LucaMarcaurelioSCTech <lmarcaurelio9@gmail.com>
2026-03-03 16:46:52 +0100

    mega fix

--- force-app/main/default/lwc/subway_surf/subway_surf.js
+++ force-app/main/default/lwc/subway_surf/subway_surf.js
@@ -1,15 +1,17 @@
 import { LightningElement } from 'lwc';
 
 export default class Subway_surf extends LightningElement {
-    videoUrl = "https://www.youtube.com/embed/vTfD20dbxho?autoplay=1&mute=1&loop=1&playlist=vTfD20dbxho&controls=0&rel=0&playsinline=1";
-    
-    currentSrc = this.videoUrl;
+    videoHidden = false;
+    iframeClass='no-click'
 
-    handleClick() {
-        if (this.currentSrc !== "") {
-            this.currentSrc = ""; 
-        } else {
-            this.currentSrc = this.videoUrl; 
+    handleClick(){
+        if (this.videoHidden) {
+            this.iframeClass = 'no-click hiddenVideo';
+            this.videoHidden = false;
+        }
+        else {
+            this.iframeClass = 'no-click';
+            this.videoHidden = true;
         }
     }
 }
