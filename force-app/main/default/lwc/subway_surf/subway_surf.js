951b74c25ef8fc79f7dff30715ab183b19dddd88
LucaMarcaurelioSCTech <lmarcaurelio9@gmail.com>
2026-03-03 16:21:07 +0100

    updated button

--- force-app/main/default/lwc/subway_surf/subway_surf.js
+++ force-app/main/default/lwc/subway_surf/subway_surf.js
@@ -3,13 +3,12 @@ import { LightningElement } from 'lwc';
 export default class Subway_surf extends LightningElement {
 
 
-    handleClick() {
-        const iframe = this.template.querySelector('iframe');
-        if(iframe.src == "https://www.youtube.com/embed/vTfD20dbxho?autoplay=1&mute=1&loop=1&playlist=vTfD20dbxho&controls=0&rel=0&playsinline=1") {
-            iframe.src=""
-        }else {
-            iframe.src = "https://www.youtube.com/embed/vTfD20dbxho?autoplay=1&mute=1&loop=1&playlist=vTfD20dbxho&controls=0&rel=0&playsinline=1"
-        }
-        
-    }
+    isShowing = true;
+    get containerClass() {
+        return this.isShowing ? 'video-visible' : 'video-hidden';
+}
+
+    toggleVideo() {
+        this.isShowing = !this.isShowing;
+}
 }
