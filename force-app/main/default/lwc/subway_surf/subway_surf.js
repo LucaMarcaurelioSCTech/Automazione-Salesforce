bbc01a4f6b134cda6125faf01643409b9d3891fc
LucaMarcaurelioSCTech <lmarcaurelio9@gmail.com>
2026-03-03 16:05:53 +0100

    added on/off button

--- force-app/main/default/lwc/subway_surf/subway_surf.js
+++ force-app/main/default/lwc/subway_surf/subway_surf.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class Subway_surf extends LightningElement {}
+export default class Subway_surf extends LightningElement {
+
+
+    handleClick() {
+        if(src == "https://www.youtube.com/embed/vTfD20dbxho?autoplay=1&mute=1&loop=1&playlist=vTfD20dbxho&controls=0&rel=0&playsinline=1") {
+            src=""
+        }else {
+            src = "https://www.youtube.com/embed/vTfD20dbxho?autoplay=1&mute=1&loop=1&playlist=vTfD20dbxho&controls=0&rel=0&playsinline=1"
+        }
+        
+    }
+}
